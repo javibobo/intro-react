cf6931d361b8615936b287920e6a551d6994c659
Javier Fernandez Heras <jfh.bobo@gmail.com>
2020-06-04 00:37:58 +0200

    Updates

---
+++ precache-manifest.9fc74587a0227e9a2ac89139900fbe82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19198702e310513c719be6ee5fdb7da",
+    "url": "/intro-react/index.html"
+  },
+  {
+    "revision": "23e4d485360f7967f848",
+    "url": "/intro-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b3feee2f19a5bc96b0e2",
+    "url": "/intro-react/static/js/2.6fa26fd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/intro-react/static/js/2.6fa26fd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23e4d485360f7967f848",
+    "url": "/intro-react/static/js/main.9029f406.chunk.js"
+  },
+  {
+    "revision": "f8f3b102770f00ee80c8",
+    "url": "/intro-react/static/js/runtime-main.16074cb7.js"
+  }
+]);
